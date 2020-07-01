ab950c1894d549c55770c4d589de5e98b3e584fb
YTK <yu.nakkano@gmail.com>
2020-07-01 21:24:47 +0900

    パスワードの最低文字数を７文字に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 7..128
   has_one :profile, dependent: :destroy
   has_one :sending_destination, dependent: :destroy
   has_one :credit_card, dependent: :destroy
