5527154c919c844fbfa65d609a74dc6d7f9e4b82
YTK <yu.nakkano@gmail.com>
2020-07-01 16:41:37 +0900

    Profileモデルのvalidationをbirthdayのみのものへ修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user, optional: true
   validates :first_name, :family_name, 
             :first_name_kana, :family_name_kana, 
-            :birth_year, :birth_month, :birth_day, presence: true
+            :birthday, presence: true
 end
