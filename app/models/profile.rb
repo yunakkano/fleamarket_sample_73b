c6a9d88fd512f39d03e84d4264b6beb6d1a1a541
YTK <yu.nakkano@gmail.com>
2020-07-01 10:27:35 +0900

    ユーザプロフィールのvalidationを追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :first_name, :family_name, 
+            :first_name_kana, :family_name_kana, 
+            :birth_year, :birth_month, :birth_day, presence: true
 end
