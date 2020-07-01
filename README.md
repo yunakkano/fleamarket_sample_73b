0f0fb83863807bf5c8ad7a621163a677c4a99662
YTK <yu.nakkano@gmail.com>
2020-07-01 16:13:19 +0900

    ユーザプロフィールに誕生年、月、日が別カラムに定義されているところ、birthdayカラム（date型）に集約

--- README.md
+++ README.md
@@ -22,9 +22,7 @@
 |family_name|string|null: false|
 |first_name_kana|string|null: false|
 |family_name_kana|string|null: false|
-|birth_year|date|null: false|
-|birth_month|date|null: false|
-|birth_day|date|null: false|
+|birthday|date|null: false|
 |user_id|references|null: false, foreign_key: true|
 
 ### Association
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_130914) do
+ActiveRecord::Schema.define(version: 2020_07_01_070915) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand", null: false
@@ -100,9 +100,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_130914) do
     t.string "family_name", null: false
     t.string "first_name_kana", null: false
     t.string "family_name_kana", null: false
-    t.date "birth_year", null: false
-    t.date "birth_month", null: false
-    t.date "birth_day", null: false
+    t.date "birthday", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
