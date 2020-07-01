39068517531e755a8a1e1106a9cdd387bac808c3
YTK <yu.nakkano@gmail.com>
2020-07-01 13:37:06 +0900

    ProfileモデルのUserテーブルへの外部参照にoptional: trueを付加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :first_name, :family_name, 
             :first_name_kana, :family_name_kana, 
             :birth_year, :birth_month, :birth_day, presence: true
