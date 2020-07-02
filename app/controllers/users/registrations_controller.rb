c0add4ce9569295ebd97f02df170c8272b60ff5b
YTK <yu.nakkano@gmail.com>
2020-07-02 10:05:09 +0900

    ウィザード形式でユーザプロフィールに加えて送付先住所を登録するよう、create_profile_addressアクションを定義

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,17 +17,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
     @profile = @user.build_profile
-    render :new_profile
+    @address = @user.build_sending_destination
+    render :new_profile_address
   end
 
-  def create_profile
+  def create_profile_address
     @user = User.new(session["devise.regist_data"]["user"])
     @profile = Profile.new(profile_params)
+    @address = SendingDestination.new(address_params)
+
     unless @profile.valid?
       flash.now[:alert] = @profile.errors.full_messages
-      render :new_profile and return
+      render :new_profile_address and return
     end
+
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_profile_address and return
+    end
+
     @user.build_profile(@profile.attributes)
+    @user.build_sending_destination(@address.attributes)
     @user.save
     session["devise.regist_data"]["user"].clear
     sign_in(:user, @user)
@@ -42,23 +52,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     )
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def address_params
+    params.require(:sending_destination).permit(
+      :dest_first_name,       :dest_family_name,
+      :dest_first_name_kana,  :dest_family_name_kana,
+      :building_name,         :post_code,
+      :prefecture_code,       :house_number,
+      :phone_number
+    )
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
