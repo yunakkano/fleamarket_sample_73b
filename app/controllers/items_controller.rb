971774a7ac07da2804da1b583323a9ff1bc4a873
ryomakitahara <r-kitahara.0819@i.softbank.jp>
2020-07-02 22:07:23 +0900

    test3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,22 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Item.includes(:images).order('created_at DESC')
+    @items = Item.includes(:item_imgs).order('created_at DESC')
   end
 
+  def new
+    @item = Item.new
+    @item.Item_img.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   has_many :item_imgs, dependent: :destroy
+  accepts_nested_attributes_for :item_imgs
   belongs_to :category
   belongs_to :brand
-  belongs_to_active_hash :item_condition
-  belongs_to_active_hash :postage_payer
-  belongs_to_active_hash :preparation_day
-  belongs_to_active_hash :postage_type
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :preparation_day
+  belongs_to :postage_type
   belongs_to :seller, class_name: "user"
   belongs_to :buyer, class_name: "user"
 end
