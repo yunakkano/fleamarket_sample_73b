1d1a572dfe65cc11ecc9d07c030dd1ae54c33718
murci-dotnet <mo-gyu@murci.net>
2020-07-13 21:51:30 +0900

    card_regist_before_rebase
    商品購入機能、カード登録
    OK : トークン発行、顧客ID生成
    NG : cardテーブルへの保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+  get 'purchase/done'
   get 'card/new'
   get 'card/show'
   devise_for :users, controllers: {
@@ -18,6 +20,13 @@ Rails.application.routes.draw do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
+    end
+  end
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
     end
   end
 end
