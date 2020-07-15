f0f6623d280e5eb2831eae72c3e5719022197421
murci-dotnet <mo-gyu@murci.net>
2020-07-15 11:57:52 +0900

    purchases_r1

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,31 @@
+class PurchasesController < ApplicationController
+
+  require 'payjp'
+
+  def index
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "cards", action: "new"
+    else
+      Payjp.api_key = "sk_test_b846970e4339aacd4d2503c7"
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def pay
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = "sk_test_b846970e4339aacd4d2503c7"
+    Payjp::Charge.create(
+    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :customer => card.customer_id, #顧客ID
+    :currency => 'jpy', #日本円
+  )
+  redirect_to action: 'done' #完了画面に移動
+  end
+
+end
