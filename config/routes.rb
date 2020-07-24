c00e0dbbd186fdf6f5be73f1714176993a61451d
murci-dotnet <mo-gyu@murci.net>
2020-07-24 15:47:02 +0900

    商品購入：登録したカードから購入、商品購入確認画面フロント、マイページからカード登録

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,36 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
   }
   devise_scope :user do
-    get  'sending_destinations', to: 'users/registrations#new_address'
-    post 'sending_destinations', to: 'users/registrations#create_address'
+    get 'profiles', to: 'users/registrations#new_profile_address'
+    post 'profiles', to: 'users/registrations#create_profile_address'
   end
 
   root 'items#index'
 
-  resources :items, except: :show
+  # resources :items, except: :show
+  resources :items do
+    collection do
+      get  'done', to:'items#done'
+    end
+    member do
+      get "purchase"
+      post "pay"
+    end
+  end
   resources :users, only: :show
+  resources :cards, only: [:new, :show] do
+    collection do
+      # get 'index', to: 'cards#index'
+      post 'show', to: 'cards#show'
+      post 'pay', to: 'cards#pay'
+      post 'delete', to: 'cards#delete'
+    end
+  end
+  resources :purchases, only: [:index] do
+    collection do
+      get 'index', to: 'purchases#index'
+      post 'pay', to: 'purchases#pay'
+      get 'done', to: 'purchases#done'
+    end
+  end
 end
