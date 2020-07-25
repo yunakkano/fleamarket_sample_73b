3c67637dd67e15ed4d70fac6c1ff00cce9985b4a
YTK <yu.nakkano@gmail.com>
2020-07-25 10:48:22 +0900

    ユーザマイページからログアウト確認ページへの遷移のためlogtout_confirmationのrouteを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   devise_scope :user do
     get  'sending_destinations', to: 'users/registrations#new_address'
     post 'sending_destinations', to: 'users/registrations#create_address'
+    get 'logout_confirmation', to: 'users/sessions#logout_confirmation'
   end
 
   root 'items#index'
