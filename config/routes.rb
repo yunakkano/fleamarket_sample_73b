95d48d8094948f53596b0ee899019550d7a45ae3
YTK <yu.nakkano@gmail.com>
2020-07-30 14:45:10 +0900

    ユーザーマイページとログアウト、クレジットカードに関連するroutingを見直し、CRUDアクションのみでコントローラが書けるよう編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'cards/new'
-  get 'cards/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -8,17 +6,23 @@ Rails.application.routes.draw do
   devise_scope :user do
     get  'sending_destinations', to: 'users/registrations#new_address'
     post 'sending_destinations', to: 'users/registrations#create_address'
-    get 'logout_confirmation', to: 'users/sessions#logout_confirmation'
+    get  '/users/:user_id/sessions', to: 'users/sessions#index'
   end
 
   root 'items#index'
 
   resources :items, except: :show
   resources :users, only: :show
-  get '/users/:id/credit_card', to: 'users#credit_card'
-  resources :cards, only: [:new, :show] do
-    collection do
-      post 'show', to: 'cards#show'
+  resources :users do
+    scope module: :users do
+      resources :sessions, only: [:index, :destroy]
+      resources :cards,    only: [:index, :new, :create]
     end
   end
+  # get '/users/:id/credit_card', to: 'users#credit_card'
+  # resources :cards, only: [:new, :show] do
+  #   collection do
+  #     post 'show', to: 'cards#show'
+  #   end
+  # end
 end
