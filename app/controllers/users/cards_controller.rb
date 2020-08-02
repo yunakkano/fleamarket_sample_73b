f0a4a23a16906a49b6b7813b1f4b34dd7ae353d9
YTK <yu.nakkano@gmail.com>
2020-08-03 00:03:01 +0900

    controllers/users/cards_controller.rbにpayjpを使用したカード情報の閲覧、登録、削除アクションを追加

--- app/controllers/users/cards_controller.rb
+++ app/controllers/users/cards_controller.rb
@@ -1,16 +1,40 @@
 class Users::CardsController < ApplicationController
     before_action :set_parents, only: [:index, :new, :create]
+    before_action :set_card, only: [:index, :new, :destroy]
+
     def index
+        if @card.blank?
+            redirect_to action: :new 
+        else
+            Payjp.api_key = Rails.application.credentials[:payjp_private_key]
+            customer = Payjp::Customer.retrieve(@card.customer_id)
+            @default_card_information = customer.cards.retrieve(@card.card_id)
+        end
     end
 
     def new
+        redirect_to action: :index if @card.present?
     end
 
     def create
     end
 
+    def destroy
+        if @card.present?
+            Payjp.api_key = Rails.application.credentials[:payjp_private_key]
+            customer = Payjp::Customer.retrieve(@card.customer_id)
+            customer.delete
+            @card.delete
+        end
+        redirect_to action: :new
+    end
+
     private
     def set_parents
         @parents = Category.where(ancestry: nil)
+    end
+
+    def set_card
+        @card = Card.find_by(user_id: current_user.id)
     end
 end
