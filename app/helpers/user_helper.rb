b3c0ae7bd2abe022404a6069aea5c327a7698864
YTK <yu.nakkano@gmail.com>
2020-08-05 01:02:26 +0900

    sidemenuヘルパーを定義し、アクティブなメニューを動的にハイライトする機能を追加

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -23,4 +23,32 @@ module UserHelper
       }
     end
 
+    def sidemenu
+      {
+        "メニュー": [
+          {title:"マイページ", path: "user_path(current_user.id)", value: "99+"},
+          {title:"お知らせ", path: "user_path(current_user.id)", value: "1"},
+          {title:"いいね！一覧", path: "user_path(current_user.id)", value: ""},
+          {title:"出品する", path: "user_path(current_user.id)", value: ""},
+          {title:"下書き一覧", path: "user_path(current_user.id)", value: ""},
+          {title:"出品した商品 - 出品中", path: "user_path(current_user.id)", value: ""},
+          {title:"出品した商品 - 取引中", path: "user_path(current_user.id)", value: ""},
+          {title:"出品した商品 - 売却済み", path: "user_path(current_user.id)", value: ""},
+          {title:"購入した商品 - 取引中", path: "user_path(current_user.id)", value: ""},
+          {title:"購入した商品 - 過去の取引", path: "user_path(current_user.id)", value: ""},
+          {title:"ニュース一覧", path: "user_path(current_user.id)", value: ""},
+          {title:"評価一覧", path: "user_path(current_user.id)", value: ""},
+          {title:"ガイド", path: "user_path(current_user.id)", value: ""},
+          {title:"お問い合わせ", path: "user_path(current_user.id)", value: ""},
+        ],
+        "設定": [
+          {title:"プロフィール", path: "user_path(current_user.id)", value: ""},
+          {title:"発送元・お届け先住所変更", path: "user_path(current_user.id)", value: ""},
+          {title:"支払い方法", path: "user_cards_path(current_user)", value: ""},
+          {title:"メール/パスワード", path: "user_path(current_user.id)", value: ""},
+          {title:"本人情報", path: "user_path(current_user.id)", value: ""},
+          {title:"電話番号の確認", path: "user_path(current_user.id)", value: ""},
+        ] 
+      }
+    end
 end  
