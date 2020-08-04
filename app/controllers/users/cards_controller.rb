fe32b607d3f082a96259bf8739ad99b9af5a493e
YTK <yu.nakkano@gmail.com>
2020-08-04 23:32:57 +0900

    カード登録に伴うコントローラの修正
    users/cards/index でカード情報が空ならカード追加確認、カード情報が存在すればカード情報を表示させる
    
    users/cards/new payjpを利用したカード登録フォームにCardインスタンスを渡す
    
    users/cards/create カード登録フォームから提供されたカードトークンを受け取り、CardインスタンスをDBに保存する（カード情報そのものの処理はpayjpに委託する）

--- app/controllers/users/cards_controller.rb
+++ app/controllers/users/cards_controller.rb
@@ -1,11 +1,11 @@
 class Users::CardsController < ApplicationController
     before_action :set_parents, only: [:index, :new, :create]
-    before_action :set_card, only: [:index, :new, :destroy]
+    before_action :set_card, only: [:index, :destroy]
+
+    require "payjp"
 
     def index
-        if @card.blank?
-            redirect_to action: :new 
-        else
+        if @card.present?
             Payjp.api_key = Rails.application.credentials[:payjp_private_key]
             customer = Payjp::Customer.retrieve(@card.customer_id)
             @default_card_information = customer.cards.retrieve(@card.card_id)
@@ -13,10 +13,26 @@ class Users::CardsController < ApplicationController
     end
 
     def new
-        redirect_to action: :index if @card.present?
+        @card = Card.new
     end
 
     def create
+        Payjp.api_key = Rails.application.credentials[:payjp_private_key]
+        if params[:card_token].blank?
+            redirect_to action: :new
+        else
+            customer = Payjp::Customer.create(
+                card: params[:card_token]
+                )
+            @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+            if @card.save
+                redirect_to action: :index
+                flash[:notice] = 'クレジットカードの登録ができました'
+            else
+                redirect_to action: :new
+                flash[:notice] = 'クレジットカードの登録に失敗しました'
+            end
+        end
     end
 
     def destroy
