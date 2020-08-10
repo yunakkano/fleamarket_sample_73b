d3302e7e70c5c6ab4d3d710f574afc0d3abab240
munerin2011 <sum.suzu0924@gmail.com>
2020-08-10 20:32:45 +0900

    お気に入り機能 > ルーティング修正
    不必要な記述を削除
    ※65行目があるため必要なし
    　resource :favorites, only: [:create, :destroy]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   get 'categories/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
