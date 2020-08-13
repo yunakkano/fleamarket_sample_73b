e1df082936d03df060f601ccf1a9a8c1046848e2
YTK <yu.nakkano@gmail.com>
2020-08-13 14:30:33 +0900

    SnsCredentialモデルに制約を追加

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,7 @@
 class SnsCredential < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  # 最初のFacebook登録時にはuser_idが保存されないが、
+  # これはsns_credentialを保存するタイミングではuser_idが確定していないため。
+  # この状態でsns_credentialを保存するためにSnsCredentialモデルには
+  # belongs_to user, optional: trueというオプションを追加
 end
