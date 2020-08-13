1dac07169642bf06e6aff3d51a75991d2c7812f9
YTK <yu.nakkano@gmail.com>
2020-08-13 14:30:04 +0900

    Oauthによる認証手続きを記述

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,33 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  before_action :minimum_pass_len, only: [:facebook, :google_oauth2]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def facebook
+    authorization
+  end
 
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
+  def google_oauth2
+    authorization
+  end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+  def failure
+    redirect_to new_user_registration_path
+  end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  private
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @profile = Profile.new
+    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+      sign_in_and_redirect @user, event: :authentication
+    else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+      render template: 'devise/registrations/new'
+    end
+  end
 
-  # protected
+  def minimum_pass_len
+    @minimum_password_length = User.password_length.min
+  end
 
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
