3d4b54dfb693064ff717b52fd407a39724fce4dc
YTK <yu.nakkano@gmail.com>
2020-08-13 14:26:49 +0900

    UserモデルにOauth APIからのレスポンスを受け取るメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 7..128
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2], password_length: 7..128
   has_one :profile, dependent: :destroy
   has_one :sending_destination, dependent: :destroy
   has_one :card, dependent: :destroy
@@ -9,6 +10,23 @@ class User < ApplicationRecord
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "items"
   has_many :favorites, dependent: :destroy
   has_many :favorite_items, through: :favorites, source: :item
+  has_many :sns_credentials
 
   validates :nickname, presence: true
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
