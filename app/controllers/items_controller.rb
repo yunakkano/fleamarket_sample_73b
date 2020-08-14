53d86e97457d7c7e05b0a88338536deaa99789c7
YTK <yu.nakkano@gmail.com>
2020-08-14 23:42:00 +0900

    set_parentsを読み込むアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_parents, only: [:index, :new, :create, :edit, :update]
+  before_action :set_parents, only: [:index, :new, :create, :edit, :update, :show]
   before_action :set_item, only: [:show, :purchase, :pay, :card_show, :edit, :update, :destroy]
   before_action :set_card, only: [:purchase, :pay, :card_show]
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_parents, only: [:show]
+    before_action :set_parents, only: [:show, :favorites]
 
     def new
     end
