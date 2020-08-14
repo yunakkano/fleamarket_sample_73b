6e7e23132d079699849c988e8ffcafd5ea4cdbae
YTK <yu.nakkano@gmail.com>
2020-08-14 20:30:12 +0900

    factory User.rbにidを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
-
     factory :user do
+      id                    {1}
       nickname              {"abe"}
       email                 {"kkk@gmail.com"}
       password              {"00000000"}
       password_confirmation {"00000000"}
     end
-  
-  end
+end
