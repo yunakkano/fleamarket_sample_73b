fea3ef506dc9a4193b88ee2a7f45a7b25474671b
YTK <yu.nakkano@gmail.com>
2020-08-15 16:52:52 +0900

    コンフリクト解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :redirect_if_signed_out, except: [:new]
     before_action :set_parents, only: [:show, :favorites]
 
     def new
@@ -14,5 +15,10 @@ class UsersController < ApplicationController
 
     def favorites
       Favorite.find_by(user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    private
+    def redirect_if_signed_out
+      redirect_to root_path and return unless request.referrer && user_signed_in?
     end
 end
