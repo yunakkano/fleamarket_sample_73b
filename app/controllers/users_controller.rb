5928da8e1b7aaeeb2160c6e18557c867f3be749b
YTK <yu.nakkano@gmail.com>
2020-08-15 16:28:27 +0900

    ログアウト時のリダイレクト処理をbefore_actionメソッドに集約

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
-    before_action :set_parents, only: [:show]
+  before_action :redirect_if_signed_out, except: [:new]  
+  before_action :set_parents, only: [:show]
 
     def new
     end
 
     def show
-      if not request.referrer
-        redirect_to root_path
-        return
-      end
       @user = User.includes(:sending_destination, :profile).find(current_user.id)
     end
 
     def favorites
       Favorite.find_by(user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    private
+    def redirect_if_signed_out
+      redirect_to root_path and return unless request.referrer && user_signed_in?
     end
 end
