10618f47ab7b75cee2e4b47fd545a84e0ff47486
YTK <yu.nakkano@gmail.com>
2020-08-23 16:46:27 +0900

    商品詳細の画像スライドの改善。マウスオーバーによるスライドを可能にする

--- app/assets/javascripts/owl-carousel.js
+++ app/assets/javascripts/owl-carousel.js
@@ -1,18 +1,22 @@
 $(document).on('turbolinks:load', function () {
-    if($('.itemdetail-background').length){
+    if($('.itemdetail-background').length !==0 ){
         var owl = $('.owl-carousel');
         owl.owlCarousel({
-            loop: true,
-            rewind: true,
+            loop: false,
             mouseDrag: true,
             margin:0,
-            nav:false,
+            nav:  false,
             dots: true,
+            //owl-dotsを%ul#slide-control-boxに格納し、スライドを操作できるように設定
+            dotsContainer: '#slide-control-box',
             autoWidth:true,
-            URLhashListener:true,
-            autoplayHoverPause:true,
-            startPosition: 'URLHash',
+            autoplayHoverPause:true
         });
+        //マウスオーバーだけで商品画像がスライド
+        $('.owl-dot').hover(function () {
+            owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+        },function(){});
+
         let ctrl = document.getElementById("slide-control-box");
         ctrl.addEventListener("mouseover", function( event ) {
             event.target.style.opacity = 1;
