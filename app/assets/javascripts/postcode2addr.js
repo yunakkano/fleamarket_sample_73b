3335d79875035e8513bc89f0c227aaab670e9784
YTK <yu.nakkano@gmail.com>
2020-08-24 21:51:45 +0900

    郵便番号から住所を自動入力する機能を実装

--- app/assets/javascripts/postcode2addr.js
+++ app/assets/javascripts/postcode2addr.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function(){
-    $("#user_postcode").jpostal({
+    $("#sending_destination_post_code").jpostal({
         postcode : [ "#sending_destination_post_code" ],
         address  : {
-                  "#sending_destination_prefecture_code" : "%3",
-                  "#sending_destination_city" : "%4",
-                  "#sending_destination_street_number" : "%5%6%7"
+            "#sending_destination_prefecture_code" : "%3",
+            "#sending_destination_city" : "%4",
+            "#sending_destination_street_number" : "%5%6%7"
         }
   });
 });
