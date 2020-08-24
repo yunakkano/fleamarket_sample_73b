1c32141994d8c45f080b789ef9a49aed6b8bf485
YTK <yu.nakkano@gmail.com>
2020-08-24 21:18:57 +0900

    郵便番号から住所を自動入力するようjs を記述

---
+++ app/assets/javascripts/postcode2addr.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function(){
+    $("#user_postcode").jpostal({
+        postcode : [ "#sending_destination_post_code" ],
+        address  : {
+                  "#sending_destination_prefecture_code" : "%3",
+                  "#sending_destination_city" : "%4",
+                  "#sending_destination_street_number" : "%5%6%7"
+        }
+  });
+});
